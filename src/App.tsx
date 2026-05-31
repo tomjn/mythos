e1b4cbf6ff39fb36fa75a367031ffec1c0f9f91a
Tom J Nowell <contact@tomjn.com>
2026-05-31 01:00:10 +0100

    Wrap app in ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import { MatchProvider } from '@/match/MatchContext'
+import { ThemeProvider } from '@/match/ThemeContext'
 import { MatchScreen } from '@/screens/MatchScreen'
 import { SettingsScreen } from '@/screens/SettingsScreen'
 
 export default function App() {
   return (
-    <MatchProvider>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<MatchScreen />} />
-          <Route path="/settings" element={<SettingsScreen />} />
-        </Routes>
-      </HashRouter>
-    </MatchProvider>
+    <ThemeProvider>
+      <MatchProvider>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<MatchScreen />} />
+            <Route path="/settings" element={<SettingsScreen />} />
+          </Routes>
+        </HashRouter>
+      </MatchProvider>
+    </ThemeProvider>
   )
 }
